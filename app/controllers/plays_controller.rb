75dd61a0ed3c2e20dd22a7268587ae2e6cb23c6e
Paul Traue <ptrauejr@gmail.com>
2019-05-23 16:41:00 -0500

    Starts the index action

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,6 +1,7 @@
 class PlaysController < ApplicationController
 
   def index
+    @plays = Play.all.order("created_at DESC")
   end
 
   def new
@@ -9,6 +10,12 @@ class PlaysController < ApplicationController
 
   def create
     @play = Play.new(play_params)
+
+    if @play.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   private
