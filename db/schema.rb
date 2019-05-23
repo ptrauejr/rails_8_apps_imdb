fa5d2dd6a34355b172fc32f4fa70a7b21d91fab5
Paul Traue <ptrauejr@gmail.com>
2019-05-23 15:59:54 -0500

    Adds the Play model

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20190523205817) do
+
+  create_table "plays", force: :cascade do |t|
+    t.string   "title",       limit: 255
+    t.text     "description", limit: 65535
+    t.string   "director",    limit: 255
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
 
 end
