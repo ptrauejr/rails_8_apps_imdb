469da9be34ba2711f8f405ebe63f5e4d91a39e6a
Paul Traue <ptrauejr@gmail.com>
2019-05-23 23:38:09 -0500

    Adds the category model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190524042159) do
+ActiveRecord::Schema.define(version: 20190524043711) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "plays", force: :cascade do |t|
     t.string   "title",       limit: 255
