54de32bc0d573b76716fef004d5a555b91877265
Paul Traue <ptrauejr@gmail.com>
2019-05-25 23:20:53 -0500

    Adds edit and new actions to Reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
-  before_action :find_play, only: [:new, :create, :edit, :destroy]
+  before_action :find_play
+  before_action :find_review, only: [:edit, :update, :destroy]
 
   def new
     @review = Review.new
@@ -17,6 +18,20 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @review.update(review_params)
+      redirect_to play_path(@play)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+  end
+
   private
 
   def review_params
@@ -25,6 +40,10 @@ class ReviewsController < ApplicationController
 
   def find_play
     @play = Play.find(params[:play_id])
+  end
+
+  def find_review
+    @review = Review.find(params[:id])
   end
 
 end
